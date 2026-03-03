bf68419967dc3cd2beb6c88857a851d424398285
dasarojukarthik2008-ai <dasarojukarthik2008@gmail.com>
2026-03-03 13:09:29 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-import multiply, { add, PI} from "./math.js";
-// document.writeln("Value of PI: " + PI + "<br>");
-// document.writeln("Addition (5 + 3): " + add(5,3) + "<br>");
-// document.writeln("Multiplication (4 * 6: " + multiply(4,6));
-
-document.body.innerHTML += 
-    "Value of PI : " + PI + "<br>" + 
-    "Addition (5 + 3) : " + add(5,3) + "<br>" + 
-    "Multiplication (4 x 6) : " + multiply(4,6);
